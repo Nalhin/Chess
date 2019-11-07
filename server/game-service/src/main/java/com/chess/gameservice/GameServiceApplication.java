6dc7037bca91b80232ab6caf13f6554c2b5af6ce
Nalhin <kris.wow@wp.pl>
2019-11-08 00:11:07 +0100

    Added travis and docker config for spring initializr

---
+++ server/game-service/src/main/java/com/chess/gameservice/GameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chess.gameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServiceApplication.class, args);
+	}
+
+}
