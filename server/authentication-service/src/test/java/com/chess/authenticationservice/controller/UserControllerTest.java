845b5648b212b26b878e1dda228302aa725fbb0d
nalhin <krol.wow@gmail.com>
2019-12-27 23:47:11 +0100

    Added h2 database for tests.

--- server/authentication-service/src/test/java/com/chess/authenticationservice/controller/UserControllerTest.java
+++ server/authentication-service/src/test/java/com/chess/authenticationservice/controller/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @Autowired
