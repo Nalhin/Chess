ab15bd37e43e40a71a7c4a3cac30a3a3bba720a8
nalhin <krol.wow@gmail.com>
2019-12-29 20:40:11 +0100

    Added zuul discovery.

---
+++ server/gateway-service/src/main/java/com/chess/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.chess.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
