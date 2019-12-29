f976c6688f1c46f93e60282c116ddb13eb3c7494
nalhin <krol.wow@gmail.com>
2019-12-29 20:31:53 +0100

    Added eureka server.

--- server/authentication-service/src/main/java/com/chess/authenticationservice/AuthenticationServiceApplication.java
+++ server/authentication-service/src/main/java/com/chess/authenticationservice/AuthenticationServiceApplication.java
@@ -2,9 +2,10 @@ package com.chess.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
--- server/chat-service/src/main/java/com/chess/chatservice/ChatServiceApplication.java
+++ server/chat-service/src/main/java/com/chess/chatservice/ChatServiceApplication.java
@@ -2,8 +2,10 @@ package com.chess.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ChatServiceApplication {
 
     public static void main(String[] args) {
